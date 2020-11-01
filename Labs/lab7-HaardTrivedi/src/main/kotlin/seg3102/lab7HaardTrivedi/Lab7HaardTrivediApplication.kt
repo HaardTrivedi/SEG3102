c2565901256e71c3c74337a65a6ecd41e878fccf
Haard Trivedi <trivedi.haard99@gmail.com>
2020-10-31 21:07:22 -0400

    Lab 7 Added

---
+++ Labs/lab7-HaardTrivedi/src/main/kotlin/seg3102/lab7HaardTrivedi/Lab7HaardTrivediApplication.kt
@@ -0,0 +1,11 @@
+package seg3102.lab7HaardTrivedi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab7HaardTrivediApplication
+
+fun main(args: Array<String>) {
+	runApplication<Lab7HaardTrivediApplication>(*args)
+}
